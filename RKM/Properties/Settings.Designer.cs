78ea32daf346c29ef97c4d7324401bbac4a7e521
Jovica Janjic <jovica.janjic@vincit.fi>
2019-02-10 00:05:49 +0100

    connStringZa2019

--- RKM/Properties/Settings.Designer.cs
+++ RKM/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RKM.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -116,7 +116,7 @@ namespace RKM.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=87.237.204.173;Initial Catalog=ELBS_2018")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=87.237.204.173;Initial Catalog=ELBS_2019")]
         public string ELBS_ConnString {
             get {
                 return ((string)(this["ELBS_ConnString"]));
